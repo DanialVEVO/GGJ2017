bbe39287d3a88da19d6e3f085f3d0c2b5131461e
MarijnvanKrieken <m.vankrieken@hotmail.com>
2017-01-21 00:12:37 +0100

    first scripts and grey matter

---
+++ Project_Bunny/ProjectBunny/Assets/Scripts/BunnyBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    float MaxSpeed = 6;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void NaarVoren()
+    {
+        if (GetComponent<Rigidbody>().velocity.z < -MaxSpeed) ;
+    }
+
+
+	void FixedUpdate () {
+        NaarVoren();
+	}
+}
